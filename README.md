d43baf0ece1149e9373400d047d5eafee215c8bd
zdc <zdc6666@163.com>
2023-02-22 21:47:40 +0800

    20230222

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # m3u8download
+## m3u8download是一个简易的根据提供的m3u8文件网址分段下载短视频的工具.可以下载常见的m3u8格式文件,使用AES.MODE_CBC解码方式对含有key的m3u8文件进行解密下载.操作简便,设置参数少
+## 使用方法
+### 例如m3u8地址为  https://v6.cdtlas.com/20220801/HPeWT1KN/hls/index.m3u8
+### 将下载的短文件存储在当前目录下一个新的文件夹命名为test
+    m3u8url = r'https://v6.cdtlas.com/20220801/HPeWT1KN/hls/index.m3u8'
+    name = "text"
+
 
--- m3u8download.py
+++ m3u8download.py
@@ -1,21 +1,16 @@
 # -*- coding：utf-8 -*-
 import os
-import shutil
 import requests
 import re
 import time
 import random  # 随机数
 import datetime
 from Crypto.Cipher import AES  # pip install pycryptodome(只限windows)
-import glob
 
-m3u8url = r'https://v6.cdtlas.com/20220801/HPeWT1KN/hls/index.m3u8'
-name = "text"
-gettss(m3u8url, name)
-def m3u8download(m3u8url, name):
+def m3u8download(m3u8url, filename):
     rstr = r"[\/\\\:\*\?\"\<\>\|\r\n]" # '/ \ : * ? " < > |'    https://blog.csdn.net/weixin_39880490/article/details/113642415
     #rstr = r'[\\/:*?"<>|\r\n]+'#在[]中*不需要转义,此时*不表示多次匹配,就表示本身的字符
-    name = re.sub(rstr, "_", name)# 替换为下划线
+    filename = re.sub(rstr, "_", filename)# 替换为下划线
     start = datetime.datetime.now().replace(microsecond=0)
     headers = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Mobile Safari/537.36"}
     m3u8 = requests.get(m3u8url.replace('"', ''), headers=headers, stream=True, allow_redirects=True)
@@ -82,31 +77,3 @@ def m3u8download(m3u8url, name):
 name="text"
 merge_to_mp4(name, delete=False)
 
-def merge_to_mp4(name, delete=False):
-    rstr = r"[\/\\\:\*\?\"\<\>\|\r\n]" # '/ \ : * ? " < > |'    https://blog.csdn.net/weixin_39880490/article/details/113642415
-    #rstr = r'[\\/:*?"<>|\r\n]+'#在[]中*不需要转义,此时*不表示多次匹配,就表示本身的字符
-    name = re.sub(rstr, "_", name)# 替换为下划线
-    # glob.glob(source_path + '/*.mp4')筛选mp4结尾的文件，按照修改时间排序
-    # files = sorted(glob.glob(source_path + '/*.mp4'),key=os.path.getmtime)
-    # source_path='./dpcq/'+ 'demo1'
-    # print(source_path)
-    # aaa=glob.glob(source_path + '/*.mp4')
-    # 筛选mp4结尾的文件，按照数字排序
-    mp4path = f'./下载视频/{name}/'
-    if not os.path.exists(mp4path):
-        os.makedirs(mp4path)
-    if os.path.exists(f'./下载视频/{name}/{name}.mp4'):
-        os.remove(f'./下载视频/{name}/{name}.mp4')
-    with open(f'./下载视频/{name}/{name}.mp4', 'wb') as fw:
-        files = sorted(glob.glob(f'./{name}/' + '*.mp4'),
-                       key=lambda x: int(x.split('\\ts')[1].split('.mp4')[0]))  # int(x[4:-4])指文件名 数字 所在的起始 结束 位置
-        for file in files:
-            print(file)
-            with open(file, 'rb') as fr:
-                fw.write(fr.read())
-                print(f'\r{file} Merged! Total:{len(files)}', end="     ")
-            if delete:
-                os.remove(file)#删除文件
-        if delete:
-            os.rmdir(f'./{name}/')#删除文件夹,文件夹需要为空
-            #shutil.rmtree(f'./{name}/')#可以删除带文件的文件夹
